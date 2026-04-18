8125360f18f726f4c87ebe1192ab5526e87895c5
prasannapandiyarajan <prasanna@codifi.in>
2026-04-18 21:17:02 +0530

    Conftest file config changed

--- pythonSel/conftest.py
+++ pythonSel/conftest.py
@@ -4,7 +4,8 @@ import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 driver = None
 
